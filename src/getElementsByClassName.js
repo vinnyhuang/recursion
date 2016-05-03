473bafbf24667629461bcf4f3f0c837d14b85ecc
Vincent Huang <vincent.vwh.huang@gmail.com>
2016-05-03 10:07:09 -0400

    Wrote getElementsByClassName, which returns an array of the html elements in a document that are of a specific class.  Passes the specRunner test. Code that is commented out comprise my attempt to allow the function to take in multiple classes, but I have yet to get this to work.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,33 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+    //var classes = className.split(" ");
+    var result = [];
+    
+    var searchTree = function(node) {
+        if(node === undefined)
+            return;
+        
+        /*var hasClass = true;
+        for(var i = 0; i < classes.length; i++) {
+            if(!(node.classList.contains(classes[i])))
+                hasClass = false;
+        }
+        if(hasClass)
+            result.push(node);*/
+
+        if(node.classList.contains(className))
+            result.push(node);
+        
+        var children = node.childNodes;
+        for(var j = 0; j < children.length; j++) {
+            searchTree(node.children[j]);
+        }
+
+        return;
+    }
+
+    searchTree(document.body);
+    return result;
 };
